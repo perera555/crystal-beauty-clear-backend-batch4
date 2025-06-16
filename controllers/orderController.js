b1114d818db6ec33a0035aa67c515e2e9ce3c08f
Dhanushka <pthamara@rocketmail.com>
2025-06-16 12:02:41 +0530

    create getOrder list 😊

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -23,31 +23,71 @@ export function createOrder(req, res) {
         date: -1
     }).limit(1).then((lastBills) => {
 
-        if(lastBills.length == 0){
+        if (lastBills.length == 0) {
             orderData.orderId = "ORD0001";
-}else {
+        } else {
 
-    const lastBill = lastBills[0];
+            const lastBill = lastBills[0];
 
-    const lastOrderId = lastBill.orderId; // its comes String("ORD0061")
-    const lastOrderNumber = lastOrderId.replace("ORD", "");//"0061"
-    const lastOrderNumberInt = parent(lastOrderNumber);//61
-    const newOrderNumberInt = lastOrderNumberInt + 1; //62
-    const newOrderNumberStr = newOrderNumberInt.toString().padStart(4, '0')//0062
-    orderData.orderId = "ORD" + newOrderNumberStr;
+            const lastOrderId = lastBill.orderId; // its comes String("ORD0061")
+            const lastOrderNumber = lastOrderId.replace("ORD", "");//"0061"
+            const lastOrderNumberInt = parent(lastOrderNumber);//61
+            const newOrderNumberInt = lastOrderNumberInt + 1; //62
+            const newOrderNumberStr = newOrderNumberInt.toString().padStart(4, '0')//0062
+            orderData.orderId = "ORD" + newOrderNumberStr;
+        }
+
+
+
+        const order = new Order(orderData);
+
+
+
+
+        order.save().then(() => {
+            res.json({
+                message: "order Saved Succefully",
+            })
+        }).catch((err) => {
+            console.log(err)
+            res.status(500).json({
+                message: "order not saved"
+            })
+        })
+
+    })
+
+
 }
-const order = new Order(orderData);
-order.save().then(() => {
-    res.json({
-        message: "order Saved Succefully",
-    })
-}).catch((err) => {
-    console.log(err)
-    res.status(500).json({
-        message: "order not saved"
-    })
-})
+export function getOrders(req,res){
+    if(req.user == null){
+        res.status(401).json({
+            message : "Unauthorized"
+        })
+        return;
+    }
+    if(req.user.role == "admin"){
+        Order.find().then(
+            (orders)=>{
+                res.json(orders)
+            }
+        ).catch((err)=>{
+            res.status(500).json({
+                message :"orders not found"
+            })
+        })
 
+    }else{
+        Order.find({
+            email : req.user.email
+        }).then((orders)=>{
+            res.json(orders)
+        }
+    ).catch((err)=>{
+        res.status(500).json({
+            message:"order not found"
+        })
     })
-
-}
+    }
+        
+    }
